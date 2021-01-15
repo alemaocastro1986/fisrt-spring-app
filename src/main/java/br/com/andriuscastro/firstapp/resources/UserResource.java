5edaa694b4fd3b0fa388a528c7a73cb9c0b3bf16
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2021-01-15 15:33:57 -0300

    feature: Create User entity and resource

---
+++ src/main/java/br/com/andriuscastro/firstapp/resources/UserResource.java
@@ -0,0 +1,19 @@
+package br.com.andriuscastro.firstapp.resources;
+
+import br.com.andriuscastro.firstapp.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "john doe", "johndoe.@teste.com", "51995179708", "132456");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
