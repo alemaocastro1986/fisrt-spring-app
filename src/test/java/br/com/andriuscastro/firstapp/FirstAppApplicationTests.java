c4d397e2740c2e5e61b41eb59873276f1fc79997
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2021-01-15 14:30:01 -0300

    first commit

---
+++ src/test/java/br/com/andriuscastro/firstapp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andriuscastro.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
